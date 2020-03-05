2c9b72abd80cfbadf15e8faaff3d4b147f7ee409
samburgess <saburgess239@gmail.com>
2020-03-05 13:00:48 -0800

    Updates

---
+++ precache-manifest.e9109ede2adaaa3a40541ac7abcde129.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e4650e8b18cf37bbaa1e1fc5eabf0e",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "0c28baaf54dfe8c2a900",
+    "url": "/mysite/static/css/main.3c4b25dd.chunk.css"
+  },
+  {
+    "revision": "09018b44a8ae473c9180",
+    "url": "/mysite/static/js/2.457e7002.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mysite/static/js/2.457e7002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c28baaf54dfe8c2a900",
+    "url": "/mysite/static/js/main.2959b326.chunk.js"
+  },
+  {
+    "revision": "18d14e6d8c63d5942777",
+    "url": "/mysite/static/js/runtime-main.add9e303.js"
+  },
+  {
+    "revision": "512d74bd31362812162541b7e187575f",
+    "url": "/mysite/static/media/7Tn.512d74bd.gif"
+  },
+  {
+    "revision": "3fc4d835ddc9dd0adddec056ace8ffac",
+    "url": "/mysite/static/media/clipboard.3fc4d835.jpg"
+  },
+  {
+    "revision": "60900645fe687ef726a9616a41a17dd1",
+    "url": "/mysite/static/media/lambda.60900645.jpg"
+  }
+]);
