83c743566763eb40423f0841fc273924d9aca872
samburgess <saburgess239@gmail.com>
2020-06-15 14:06:05 -0700

    insiderbot src

--- src/Projects.js
+++ src/Projects.js
@@ -9,7 +9,15 @@ var generated = false;
 
 function genProjects(){
 
-    console.log("gen projects")
+    var insiderBot = {
+        title:"Insider Trading detector",
+        description: "Ongoing project to utilize deep learning methods to detect insider trading on the New York Stock Exchange. The model uses an LSTM RNN to analyze windows of stock data, giving likelihoods of fraud occurence. ",
+        technologies: "Python, PyTorch, Deep Learning",
+        pages_link:'',
+        source_link:'https://github.com/samburgess/insiderbot'
+    }
+
+    projects.push(insiderBot)
 
     var covidmap = {
         title:'Covid 19 Visualisation',
@@ -24,9 +32,9 @@ function genProjects(){
     //dirtbag
     var dirtbag = {
         title:'Dirtbag',
-        description:"React app utilizing REI's Mountain Project API to find climbing routes near the user`s geolocation.",
+        description:"React app utilizing REI's Mountain Project API to find climbing routes near the user's geolocation.",
         technologies:'JavaScript, React.js',
-        pages_link: '',
+        pages_link: 'https://samburgess.github.io/dirtbag',
         source_link: 'https://github.com/samburgess/dirtbag'
     }
 
@@ -36,15 +44,14 @@ function genProjects(){
         title:'WWU Content Aggregation System',
         description:'A system that pulls content (articles etc.) from various University publications into a central database. The database is accompanied by a web portal that allows whitelisted users to search our database and create collections of content piece for projects. ',
         technologies:'JavaScript, Node.js, React.js, Next.js, MySQL, PHP ',
-        pages_link:null,
-        source_link:null
+        pages_link:'',
+        source_link:''
     }
 
     projects.push(webtech)
 
+
     generated = true;
-
-
 }
 
 class Project extends React.Component{
@@ -57,14 +64,18 @@ class Project extends React.Component{
         return(
             <div className="Project">
                 <h1>{this.props.title}</h1>
+                {this.props.source_link === '' ? null : 
                 <a target="_blank" href={this.props.source_link} className="Project-link">
                     View Source on Github >
                 </a>
+                }
+                {this.props.pages_link === '' ? null : 
                 <a target="_blank" href={this.props.pages_link} className="Project-link">
                     View on Github Pages >
                 </a>
-                <h2>{this.props.technologies}</h2>
-                <div>{this.props.description}</div>
+                }
+                <h4 style={{letterSpacing:'2px'}}>{this.props.technologies}</h4>
+                <div style={{letterSpacing:'0px'}}>{this.props.description}</div>
             </div>
     )}
 
