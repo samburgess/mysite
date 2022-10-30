6e7c8e91555392b725e6994d0646d9144c3fc424
sam <saburgess239@gmail.com>
2022-10-29 18:53:20 -0700

    about me tweaks

--- src/About.js
+++ src/About.js
@@ -7,12 +7,14 @@ function About(){
         <div className="About">
             <h1>About Me</h1>
             <div style={{letterSpacing:'0px'}}>
-            Hi! My name is Sam, I am a Software Developer, currently looking to transition back into a Software Development role after spending some time in the outdoor industry.
+            Hi! My name is Sam, I am a Software Developer currently looking to transition back into a Software Development role after spending some time in the outdoor industry.
             I have a bachelor's in Computer Science from Western Washington University, and a real passion for creating high quality products.
-            I especially enjoy working with web facing technologies, and big data sets!
-            On this site you can find some examples of my work, some of which you can run in the browser. Thanks for stopping by :)
+            I especially enjoy working with web facing technologies, and big data sets.
+            <br />
+            <br />
+            On this site you can find examples of my work, some of which you can run in the browser. Thanks for stopping by!
 
-            </div>
+        </div>
         </div>
 
 )}
