e8da64a341f85560b11b10725a99def7ff1d4b3b
sam <saburgess239@gmail.com>
2022-10-29 17:37:24 -0700

    about me update2

--- src/About.js
+++ src/About.js
@@ -10,6 +10,7 @@ function About(){
             Hi! My name is Sam, I am a Software Developer, currently looking to transition back into a Software Development role after spending some time in the outdoor industry.
             I have a bachelor's in Computer Science from Western Washington University, and a real passion for creating high quality products.
             I especially enjoy working with web facing technologies, and big data sets!
+            On this site you can find some examples of my work, some of which you can run in the browser. Thanks for stopping by :)
 
             </div>
         </div>
